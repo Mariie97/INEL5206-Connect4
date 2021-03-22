c5ee1252cc19ce19d01b2fea66d714c261074ecf
Mariie97 <coralys.cortes1@upr.edu>
2021-03-22 19:51:05 -0400

    Add input signal: invalid_move and board_full

--- Connect4/FSM.v
+++ Connect4/FSM.v
@@ -4,7 +4,7 @@ module FSM (
      input clk,// clock of the circuit 
      input reset,// reset 
      input invalid_move, //Flag when column is full 
-     input [1:0] in_game_status
+     input [1:0] in_game_status,
 	  input player_turn,	  //indicate wich player turn (P1=0 P2=1)
 	  input board_full,
      output reg [1:0] out_game_status 
@@ -28,33 +28,72 @@ end
  // next state 
 always @(current_state)
 begin
-	if(board_full) next_state<=GAME_INIT 
-	case(current_state)
-		GAME_INIT: begin
-						next_state <= PI_TURN;
-						out_game_status <= STILL_PLAYING;
+	if(board_full==1'b1) begin
+									next_state<=GAME_INIT;
+									out_game_status = TIE;
+								end
+	else begin
+		case(current_state)
+			GAME_INIT: begin
+							next_state <= PI_TURN;
+							out_game_status <= STILL_PLAYING;
+							end
+			P1_TURN: begin
+						if(invalid_move==1'b1)
+						begin
+							next_state <= PI_TURN;
+							out_game_status <= STILL_PLAYING;
 						end
-		P1_TURN: begin
-					case(in_game_status)
-					NEXT_TURN: begin
-									next_state <= P2_TURN;
-									out_game_status <= STILL_PLAYING;
-									end
-					GAME_OVER: begin
-									next_state <= GAME_INIT;
-									out_game_status <= 2'bXX;
-									end
-					TIE_GAME:
-					default:
-						next_state <= GAME_INIT;
-						out_game_status <= TIE;		
-					endcase
-
-		END_GAME:begin
-					end
-		
-		P2_TURN:begin // game done
-					end			
-	endcase
+						else begin							
+							case(in_game_status)
+							NEXT_TURN: begin
+											next_state <= P2_TURN;
+											out_game_status <= STILL_PLAYING;
+											end
+							GAME_OVER: begin
+											next_state <= GAME_INIT;
+											out_game_status <= 2'bXX;
+											end
+							TIE_GAME: begin
+																					
+											
+										 end
+							default: begin
+											next_state <= END_GAME;
+											out_game_status <= TIE;		
+										end
+							endcase
+							end
+						end
+			END_GAME:begin
+						end
+			
+			P2_TURN:begin
+						if(invalid_move==1'b1)
+						begin
+							next_state <= P2_TURN;
+							out_game_status <= STILL_PLAYING;
+						end
+						else begin							
+							case(in_game_status)
+							NEXT_TURN: begin
+											next_state <= P1_TURN;
+											out_game_status <= STILL_PLAYING;
+											end
+							GAME_OVER: begin
+											next_state <= GAME_INIT;
+											out_game_status <= 2'bXX;
+											end
+							TIE_GAME: begin
+										 end
+							default: begin
+											next_state <= GAME_INIT;
+											out_game_status <= TIE;		
+										end
+							endcase
+							end
+						end		
+		endcase
+	end
  end
 endmodule
