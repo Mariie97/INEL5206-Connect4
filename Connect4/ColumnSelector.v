aca01ca7e10f8ea65af8150d010d02b2c27740ea
Mariie97 <coralys.cortes1@upr.edu>
2021-04-08 16:21:07 -0400

    Add invalid column input

--- Connect4/ColumnSelector.v
+++ Connect4/ColumnSelector.v
@@ -3,34 +3,70 @@
 
 module ColumnSelector(input clk, // clock
 					input reset, // reset
+					input throw_again,
 					input [2:0] in_column, // selected column (0-6)
 					input [1:0] state,
 					output reg [41:0] out_gameboard, // keep track of selected and empty cells. 0-Empty, 1-Selected
-					output reg [41:0] out_players_cells // keep track of player cells. 0-Player1, 1-Player2 
-					//output out_invalidColumn // 0-column is valid, 1-column is invalid, is full 
-					
+					output reg [41:0] out_players_cells, // keep track of player cells. 0-Player1, 1-Player2 
+					output reg invalid_column // 0-column is valid, 1-column is invalid, is full 
     );
 	 
 
 integer i, column_index;
-
 reg flag_token_placed = 0; 
+parameter GAME_INIT=2'b00, P1_TURN=2'b01, P2_TURN=2'b10, END_GAME=2'b11; //STATES
+parameter CELLS_NUMBER = 42;
 
-always@(state)
+
+always@(state, posedge reset, posedge throw_again)
 begin	
-//	if(reset==1'b1)
-//	begin
-//		out_gameboard = 42'bX;
-//		out_players_cells = 42'bX;
-//	end
-
+$monitor("in_column %b", in_column);
+	if(reset==1'b1)
+	begin
+		out_gameboard <= 42'b0;
+		out_players_cells <= 42'bX;
+	end
+	else begin
 	case(state)
-		2'b00: out_gameboard = 42'b0; // Game init
-		2'b01: begin  //P1 turn 
+		GAME_INIT: begin // initialize data
+						out_gameboard <= 42'b0; 
+						invalid_column <= 1'b0;
+						end
+		P1_TURN: begin  //P1 turn 
 					if(out_gameboard[in_column]==1'b0)
 						begin
-							out_gameboard[in_column] = 1;
-							out_players_cells[in_column] = 0;
+							out_gameboard[in_column] <= 1;
+							out_players_cells[in_column] <= 0;
+							invalid_column <= 1'b0;
+					
+						end
+					else
+						begin
+							for(i=in_column+7; i<CELLS_NUMBER; i=i+7)
+								begin
+									if(out_gameboard[i]==1'b0 && flag_token_placed==1'b0)
+										begin
+											out_gameboard[i]<=1;
+											out_players_cells[i] <= 0;
+											assign flag_token_placed = 1;
+											invalid_column <= 1'b0;
+										end
+								end
+								if (flag_token_placed==1'b1) 
+								begin
+									assign flag_token_placed = 0;
+								end
+								else begin 
+											invalid_column <= 1'b1;						
+										end								
+						end
+					end
+			P2_TURN: begin//P2 turn 
+					if(out_gameboard[in_column]==1'b0)
+						begin
+							out_gameboard[in_column] <= 1;
+							out_players_cells[in_column] <= 1;
+							invalid_column <= 1'b0;
 						end
 					else
 						begin
@@ -38,40 +74,23 @@ begin
 								begin
 									if(out_gameboard[i]==1'b0 && flag_token_placed==1'b0)
 										begin
-											out_gameboard[i]=1;
-											out_players_cells[i] = 0;
+											out_gameboard[i] <= 1;
+											out_players_cells[i] <= 1;
 											assign flag_token_placed = 1;
+											invalid_column <= 1'b0;
 										end
 								end
-						assign flag_token_placed = 0;
-
-						end
-					end
-			2'b10: //P2 turn 
-			begin  
-					if(out_gameboard[in_column]==1'b0)
-						begin
-							out_gameboard[in_column] = 1;
-							out_players_cells[in_column] = 1;
-						end
-					else
-						begin
-							for(i=in_column+7; i<=41; i=i+7)
-								begin
-									if(out_gameboard[i]==1'b0 && flag_token_placed==1'b0)
-										begin
-											out_gameboard[i]=1;
-											out_players_cells[i] = 1;
-											assign flag_token_placed = 1;
+								if (flag_token_placed==1'b1) begin
+																assign flag_token_placed = 0;
+																end
+								else begin 
+											invalid_column <= 1'b1;						
 										end
-								end
-							assign flag_token_placed = 0;
 						end
 					end
-			//2'b11: //End Game
-	
+			//END_GAME: //End Game
 	endcase
-
+	end
 end
 endmodule
 
--- Connect4/ColumnSelector_tb.v
+++ Connect4/ColumnSelector_tb.v
@@ -1,39 +1,20 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   15:47:56 03/22/2021
-// Design Name:   ColumnSelector
-// Module Name:   C:/Users/cmcsc/Desktop/Segundo Semestre Virtual/Switching 2/Project/repo/INEL5206-Connect4/Connect4/ColumnSelector_tb.v
-// Project Name:  Connect4
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: ColumnSelector
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
-
 module ColumnSelector_tb;
 
 	// Inputs
 	reg clk;
 	reg reset;
+	reg throw_again;
 	reg [2:0] in_column;
 	reg [1:0] state;
 
 	// Outputs
 	wire [41:0] out_gameboard;
 	wire [41:0] out_players_cells;
-
+	wire invalid_column;
+	
+	
 	// Instantiate the Unit Under Test (UUT)
 	ColumnSelector uut (
 		.clk(clk), 
@@ -41,7 +22,9 @@ module ColumnSelector_tb;
 		.in_column(in_column), 
 		.state(state), 
 		.out_gameboard(out_gameboard), 
-		.out_players_cells(out_players_cells)
+		.out_players_cells(out_players_cells),
+		.invalid_column(invalid_column),
+		.throw_again(throw_again)
 	);
 
 	always #10 clk=~clk;
@@ -52,30 +35,63 @@ module ColumnSelector_tb;
 		in_column = 0;
 		state = 0;
 		reset=0;
+		throw_again = 0;
 		
 		// Wait 100 ns for global reset to finish
 		#100;
 		
+//		#10 state = 2'b01; //P1		
+//		 in_column=3'b000;
+//		
+//		#25 state = 2'b10; //P2
+//		in_column=3'b010;
+//		
+//		#25 state = 2'b01; //P1
+//		in_column=3'b000;
+//
+//		#25 state = 2'b10; //P2
+//		in_column=3'b010;
+//		
+//		#25 state = 2'b01; //P1
+//		in_column=3'b110;
+//		
+//		#25 state = 2'b10; //P2
+//		in_column=3'b000;
+//	
+//		#25 state = 2'b01; //P1
+//		in_column=3'b000;
+
 		#10 state = 2'b01; //P1		
 		 in_column=3'b000;
 		
 		#25 state = 2'b10; //P2
-		in_column=3'b010;
+		in_column=3'b000;
 		
 		#25 state = 2'b01; //P1
 		in_column=3'b000;
 
 		#25 state = 2'b10; //P2
-		in_column=3'b010;
+		in_column=3'b000;
 		
 		#25 state = 2'b01; //P1
-		in_column=3'b110;
+		in_column=3'b000;
 		
 		#25 state = 2'b10; //P2
 		in_column=3'b000;
 	
 		#25 state = 2'b01; //P1
 		in_column=3'b000;
+		#5 throw_again = 1;
+
+		in_column=3'b001;
+		#5 throw_again = 0;
+
+		
+		
+		
+		#25 reset = 2'b1;
+		
+		#50 $finish;
 
 end
 initial $monitor("column=%d", in_column);      
