68a2738231e6665d6044ee0573e046e9dfb62796
Jorge Calderon <jorge.calderon3@upr.edu>
2021-04-14 11:08:52 -0400

    Implemented detect winner module and tested that it works.

--- DetectWinner.v
+++ DetectWinner.v
@@ -17,75 +17,76 @@ begin
 //Set the state of the game to be still playing. If another condition is met it will be changed along the way.
 game_status = still_playing; 
 
+
 	//Verify first if each possible combination is met. If it is, figure out which player is responsible and then assign game_status
-	if (game_board[12] == 1 && game_board[13] == 1 && game_board[14] == 1 && game_board[15] == 1) //Horizontal First Row
+	if (game_board[12] == 1 && game_board[13] == 1 && game_board[14] == 1 && game_board[15] == 1) //Horizontal Fourth Row
 		begin
 				if (player_cells[12] == 1 && player_cells[13] == 1 && player_cells[14] == 1 && player_cells[15] == 1) 
 						game_status = p2_wins;
-				else
+				else if (player_cells[12] == 0 && player_cells[13] == 0 && player_cells[14] == 0 && player_cells[15] == 0)
 						game_status = p1_wins;
 		end
-	else if (game_board[8] == 1 && game_board[9] == 1 && game_board[10] == 1 && game_board[11] == 1) //Horizontal Second Row
+	else if (game_board[8] == 1 && game_board[9] == 1 && game_board[10] == 1 && game_board[11] == 1) //Horizontal Third Row
 		begin
 				if (player_cells[8] == 1 && player_cells[9] == 1 && player_cells[10] == 1 && player_cells[11] == 1)
 						game_status = p2_wins;
-				else
+				else if (player_cells[8] == 0 && player_cells[9] == 0 && player_cells[10] == 0 && player_cells[11] == 0)
 						game_status = p1_wins;
 		end
-	else if (game_board[4] == 1 && game_board[5] == 1 && game_board[6] == 1 && game_board[7] == 1) //Horizontal Third Row
+	else if (game_board[4] == 1 && game_board[5] == 1 && game_board[6] == 1 && game_board[7] == 1) //Horizontal Second Row
 		begin 
 				if (player_cells[4] == 1 && player_cells[5] == 1 && player_cells[6] == 1 && player_cells[7] == 1)
 						game_status = p2_wins;
-				else
+				else if (player_cells[4] == 0 && player_cells[5] == 0 && player_cells[6] == 0 && player_cells[7] == 0)
 						game_status = p1_wins;
 		end
-	else if (game_board[0] == 1 && game_board[1] == 1 && game_board[2] == 1 && game_board[3] == 1) // Horizontal Fourth Row
+	else if (game_board[0] == 1 && game_board[1] == 1 && game_board[2] == 1 && game_board[3] == 1) // Horizontal First Row
 		begin
 				if (player_cells[0] == 1 && player_cells[1] == 1 && player_cells[2] == 1 && player_cells[3] == 1) 
 						game_status = p2_wins;
-				else
+				else if (player_cells[0] == 0 && player_cells[1] == 0 && player_cells[2] == 0 && player_cells[3] == 0) 
 						game_status = p1_wins;
 		end
 	else if (game_board[12] == 1 && game_board[8] == 1 && game_board[4] == 1 && game_board[0] == 1) // Vertical First Column
 		begin
 				if (player_cells[12] == 1 && player_cells[8] == 1 && player_cells[4] == 1 && player_cells[0] == 1) 
 						game_status = p2_wins;
-				else
+				else if (player_cells[12] == 0 && player_cells[8] == 0 && player_cells[4] == 0 && player_cells[0] == 0) 
 						game_status = p1_wins;
 		end
 	else if (game_board[13] == 1 && game_board[9] == 1 && game_board[5] == 1 && game_board[1] == 1) // Vertical Second Column
 		begin
 				if (player_cells[13] == 1 && player_cells[9] == 1 && player_cells[5] == 1 && player_cells[1] == 1) 
 						game_status = p2_wins;
-				else
+				else if (player_cells[13] == 0 && player_cells[9] == 0 && player_cells[5] == 0 && player_cells[1] == 0) 
 						game_status = p1_wins;
 		end
 	else if (game_board[14] == 1 && game_board[10] == 1 && game_board[6] == 1 && game_board[2] == 1) // Vertical Third Column
 		begin
 				if (player_cells[14] == 1 && player_cells[10] == 1 && player_cells[6] == 1 && player_cells[2] == 1) 
 						game_status = p2_wins;
-				else
+				else if (player_cells[14] == 0 && player_cells[10] == 0 && player_cells[6] == 0 && player_cells[2] == 0)
 						game_status = p1_wins;	
 		end
 	else if (game_board[15] == 1 && game_board[11] == 1 && game_board[7] == 1 && game_board[3] == 1) // Vertical Fourth Column
 		begin
 				if (player_cells[15] == 1 && player_cells[11] == 1 && player_cells[7] == 1 && player_cells[3] == 1) 
 						game_status = p2_wins;
-				else
+				else if (player_cells[15] == 0 && player_cells[11] == 0 && player_cells[7] == 0 && player_cells[3] == 0)
 						game_status = p1_wins;
 		end
-	else if (game_board[12] == 1 && game_board[9] == 1 && game_board[6] == 1 && game_board[3] == 1) // Diagonal Starting from First Row, First Column going Down and Right
+	else if (game_board[12] == 1 && game_board[9] == 1 && game_board[6] == 1 && game_board[3] == 1) // Diagonal Starting from Fourth Row, First Column going Down and Right
 		begin
 				if (player_cells[12] == 1 && player_cells[9] == 1 && player_cells[6] == 1 && player_cells[3] == 1) 
 						game_status = p2_wins;
-				else
+				else if (player_cells[12] == 0 && player_cells[9] == 0 && player_cells[6] == 0 && player_cells[3] == 0) 
 						game_status = p1_wins;
 		end
-	else if (game_board[15] == 1 && game_board[10] == 1 && game_board[5] == 1 && game_board[0] == 1) // Diagonal Starting from First Row, Fourth Column going Down and Left
+	else if (game_board[15] == 1 && game_board[10] == 1 && game_board[5] == 1 && game_board[0] == 1) // Diagonal Starting from Fourth Row, Fourth Column going Down and Left
 		begin
 				if (player_cells[15] == 1 && player_cells[10] == 1 && player_cells[5] == 1 && player_cells[0] == 1) 
 						game_status = p2_wins;
-				else
+				else if (player_cells[15] == 0 && player_cells[10] == 0 && player_cells[5] == 0 && player_cells[0] == 0)
 						game_status = p1_wins;
 		end
 						
--- DetectWinnerTest.v
+++ DetectWinnerTest.v
@@ -17,6 +17,11 @@ module DetectWinnerTest;
 		.game_status(game_status)
 	);
 
+
+	
+	always #10 clk=~clk;
+	
+	
 	initial begin
 		// Initialize Inputs
 		clk = 0;
@@ -27,38 +32,165 @@ module DetectWinnerTest;
 		#100;
         
 		// Add stimulus here
-		#20 game_board[1] = 1;
-		player_cells[1] = 0; //P1
 		
-		#20 game_board[5] = 1;
-		player_cells[5] = 1; //P2
-		
-		#20 game_board[2] = 1;
-		player_cells[2] = 0; //P1
-		
-		#20 game_board[6] = 1;
-		player_cells[6] = 1; //P2
-		
-		#20 game_board[0] = 1;
+///////////// First Game (P1 Wins) ////////////////
+/*		#20 game_board[0] = 1;
 		player_cells[0] = 0; //P1
 		
 		#20 game_board[4] = 1;
 		player_cells[4] = 1; //P2
 		
+		#20 game_board[1] = 1;
+		player_cells[1] = 0; //P1
+		
+		#20 game_board[8] = 1;
+		player_cells[8] = 1; //P2
+		
+		#20 game_board[5] = 1;
+		player_cells[5] = 0; //P1
+		
+		#20 game_board[2] = 1;
+		player_cells[2] = 1; //P2
+		
+		#20 game_board[9] = 1;
+		player_cells[9] = 0; //P1
+		
+		#20 game_board[13] = 1;
+		player_cells[13] = 1; //P2
+		
 		#20 game_board[3] = 1;
 		player_cells[3] = 0; //P1
 		
+		#20 game_board[6] = 1;
+		player_cells[6] = 1; //P2
 		
+		#20 game_board[10] = 1;
+		player_cells[10] = 0; //P1
+		
+		#20 game_board[7] = 1;
+		player_cells[7] = 1; //P2
+		
+		#20 game_board[14] = 1;
+		player_cells[14] = 0; //P1
+		
+		#20 game_board[11] = 1;
+		player_cells[11] = 1; //P2
+		
+		#20 game_board[15] = 1;
+		player_cells[15] = 0; //P1
+		
+		#20 game_board[12] = 1;
+		player_cells[12] = 1; //P2*/
+//////////////// (P2 no sabe jugar xD)///////////////////		
+		
+		
+/////////// Second Game (P2 redemption) ///////////////
 
+	/*	#20 game_board[0] = 1;
+		player_cells[0] = 0; //P1
+		
+		#20 game_board[1] = 1;
+		player_cells[1] = 1; //P2
+		
+		#20 game_board[2] = 1;
+		player_cells[2] = 0; //P1
+		
+		#20 game_board[5] = 1;
+		player_cells[5] = 1; //P2
+		
+		#20 game_board[6] = 1;
+		player_cells[6] = 0; //P1
+		
+		#20 game_board[9] = 1;
+		player_cells[9] = 1; //P2
+		
+		#20 game_board[10] = 1;
+		player_cells[10] = 0; //P1
+		
+		#20 game_board[13] = 1;
+		player_cells[13] = 1; //P2*/
+		
+////////// se colgo P1 ////////////////////
+
+
+//////// Third Game (El empate) ///////////////////
+
+
+		#20 game_board[0] = 1;
+		player_cells[0] = 0; //P1
+		
+		#20 game_board[1] = 1;
+		player_cells[1] = 1; //P2
+		
+		#20 game_board[5] = 1;
+		player_cells[5] = 0; //P1
+		
+		#20 game_board[2] = 1;
+		player_cells[2] = 1; //P2
+		
+		#20 game_board[9] = 1;
+		player_cells[9] = 0; //P1
+		
+		#20 game_board[3] = 1;
+		player_cells[3] = 1; //P2
+		
+		#20 game_board[13] = 1;
+		player_cells[13] = 0; //P1
+		
+		#20 game_board[4] = 1;
+		player_cells[4] = 1; //P2
+		
+		#20 game_board[7] = 1;
+		player_cells[7] = 0; //P1
+		
+		#20 game_board[8] = 1;
+		player_cells[8] = 1; //P2
+		
+		#20 game_board[6] = 1;
+		player_cells[6] = 0; //P1
+		
+		#20 game_board[10] = 1;
+		player_cells[10] = 1; //P2
+		
+		#20 game_board[12] = 1;
+		player_cells[12] = 0; //P1
+		
+		#20 game_board[14] = 1;
+		player_cells[14] = 1; //P2
+		
+		#20 game_board[11] = 1;
+		player_cells[11] = 0; //P1
+		
+		#20 game_board[15] = 1;
+		player_cells[15] = 1; //P2
+		
+/////////// aprendieron a jugar los dos ///////
+
+
+///// Verify still playing /////
+
+/*		#20 game_board[0] = 1;
+		player_cells[0] = 0; //P1
+		
+		#20 game_board[4] = 1;
+		player_cells[4] = 1; //P2*/
+		
+//////////////////////////////////////
+
+
+
+
+		
+		#50 $finish;
 	end
+	
+	
+	 
 	
 	initial 
 	begin
-			for (int i = 0; i < 15; i += 1)
-			begin
-					$display("GameBoard: [%d]", game_board[i]);
-					$monitor("Output: %d", game_status);
-			end
+			//$monitor("GameBoard: [%b]", game_board);
+			$monitor("Output: %b", game_status);
 	end
       
 endmodule
